517fa85adebddcb4c54eea8ade66581db013cae0
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-07-23 20:57:33 -0700

    chore: Eslint parser sucks

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ var command = require.resolve('../');
 describe('update-template', function () {
   // Just a dummy test to make CI pass
   it('prints help', function (done) {
-    this.timeout(10_000);
+    this.timeout(10000);
     var help = cp.spawnSync('node', [command, '--help']);
     expect(help.stdout).toBeDefined();
     done();
