2a56bd9350504ee4a33431b8d435b8b1fbbb3b3d
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-07-23 20:54:36 -0700

    chore: Extend timeout

--- test/index.js
+++ test/index.js
@@ -6,6 +6,7 @@ var command = require.resolve('../');
 describe('update-template', function () {
   // Just a dummy test to make CI pass
   it('prints help', function (done) {
+    this.timeout(10_000);
     var help = cp.spawnSync('node', [command, '--help']);
     expect(help.stdout).toBeDefined();
     done();
